8e1d8a8c52a0601e8e48fe4d9c16007ad9d49b80
SweetieSara <saranyaamani10@gmail.com>
2022-02-05 23:51:40 +0530

    Task done

---
+++ src/org/prac/Practice.java
@@ -0,0 +1,14 @@
+package org.prac;
+
+public class Practice {
+
+	public void add() {
+		// TODO Auto-generated method stub
+System.out.println("GIT task");
+	}
+	
+	public static void main(String[] args) {
+		Practice a = new Practice();
+		a.add();
+	}
+}
